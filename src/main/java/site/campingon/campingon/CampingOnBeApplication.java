6dc602344173c183f8720b12f0d93fb1ca5ef255
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-11-20 20:23:13 +0900

    Initial Commit - Spring Setting

---
+++ src/main/java/site/campingon/campingon/CampingOnBeApplication.java
@@ -0,0 +1,13 @@
+package site.campingon.campingon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingOnBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingOnBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/campingon/campingon/CampingOnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.campingon.campingon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingOnBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
