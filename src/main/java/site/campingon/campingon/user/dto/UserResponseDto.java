78c47c5956cf42ff4378482bc9ca2f84e08c1dd7
Jihyeon Yun <jihyeon.yun619@gmail.com>
2024-11-23 17:48:57 +0900

    refactor: #28 ResponseEntity에서 Role enum으로 반환
    - String 대신 Role enum class 반환하도록 수정

--- src/main/java/site/campingon/campingon/user/dto/UserResponseDto.java
+++ src/main/java/site/campingon/campingon/user/dto/UserResponseDto.java
@@ -2,6 +2,8 @@ package site.campingon.campingon.user.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import site.campingon.campingon.user.entity.Role;
+
 
 @Getter
 @Builder
@@ -9,6 +11,6 @@ public class UserResponseDto {
     private Long id;          // 사용자 ID
     private String email;     // 이메일
     private String nickname;  // 닉네임
-    private String role;      // 권한 (USER/ADMIN 등)
+    private Role role;      // 권한 (USER/ADMIN 등)
     private boolean isDeleted; // 삭제 여부
 }
