174450636a0a172c43a82d2311170353abb3315a
이찬진 <chanjin23@naver.com>
2024-11-25 23:55:24 +0900

    feat : #31 - AOP 글로벌 로깅

--- src/main/java/site/campingon/campingon/CampingOnBeApplication.java
+++ src/main/java/site/campingon/campingon/CampingOnBeApplication.java
@@ -2,10 +2,12 @@ package site.campingon.campingon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class CampingOnBeApplication {
 
     public static void main(String[] args) {
