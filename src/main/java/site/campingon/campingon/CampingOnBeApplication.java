60bdb69ad2aeb7a00542846be2d70da1f69a43c3
이찬진 <chanjin23@naver.com>
2024-11-26 01:07:10 +0900

    refactor : #31 - @EnableAspectJAutoProxy 제거

--- src/main/java/site/campingon/campingon/CampingOnBeApplication.java
+++ src/main/java/site/campingon/campingon/CampingOnBeApplication.java
@@ -2,12 +2,10 @@ package site.campingon.campingon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class CampingOnBeApplication {
 
     public static void main(String[] args) {
