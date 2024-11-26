f2376ec4071ea43ed73fb51b5924eac0d2383a90
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-11-27 06:46:00 +0900

    style: oauth 패키지 이동, 로깅을 위해 @toString 추가

--- src/main/java/site/campingon/campingon/common/oauth/dto/provider/OAuth2ResponseDto.java
+++ src/main/java/site/campingon/campingon/common/oauth/dto/provider/OAuth2ResponseDto.java
@@ -1,4 +1,4 @@
-package site.campingon.campingon.common.config.oauth.dto.provider;
+package site.campingon.campingon.common.oauth.dto.provider;
 
 // 제공자마다 반환 형태가 달라서 interface 생성. 제공자별 구현체 필요
 public interface OAuth2ResponseDto {
