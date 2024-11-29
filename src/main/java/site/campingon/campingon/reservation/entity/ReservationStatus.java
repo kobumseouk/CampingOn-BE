47fb857ed04ee3d7950132ac222ad499f9bc5129
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-11-29 21:24:34 +0900

    refactor: 예약상태 컨버터 오류 수정(DB에 영문, Json으로는 한글로 반환)

--- src/main/java/site/campingon/campingon/reservation/entity/ReservationStatus.java
+++ src/main/java/site/campingon/campingon/reservation/entity/ReservationStatus.java
@@ -1,9 +1,9 @@
 package site.campingon.campingon.reservation.entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
 public enum ReservationStatus {
 
@@ -12,5 +12,11 @@ public enum ReservationStatus {
     COMPLETED("체크인완료"); // 체크인 이후로 (자동) 변경
 
     private final String status;
+
+    // JSON 변환 시 한글값 사용
+    @JsonValue
+    public String getStatus() {
+        return this.status;
+    }
 
 }
