cc758ca30f5c15b15cadce21b10da560d576c2fc
이찬진 <chanjin23@naver.com>
2024-11-29 17:03:47 +0900

    feat : #89 - 스프링 스케줄러 추가

--- src/main/java/site/campingon/campingon/CampingOnBeApplication.java
+++ src/main/java/site/campingon/campingon/CampingOnBeApplication.java
@@ -3,9 +3,11 @@ package site.campingon.campingon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class CampingOnBeApplication {
 
     public static void main(String[] args) {
