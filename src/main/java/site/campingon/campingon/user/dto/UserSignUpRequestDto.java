778ba6106bb4967eea3bf7b34fa7570f099093a8
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-12-02 13:09:35 +0900

    Feat: #145 길이 조건 regex에서 확인
    - 비밀번호 길이 조건 regex에서 확인
    - 닉네임 길이 조건 regex에서 확인
    
    Closes #145

--- src/main/java/site/campingon/campingon/user/dto/UserSignUpRequestDto.java
+++ src/main/java/site/campingon/campingon/user/dto/UserSignUpRequestDto.java
@@ -3,7 +3,6 @@ package site.campingon.campingon.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,14 +19,13 @@ public class UserSignUpRequestDto {
     private String email;
 
     @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
-    @Size(min = 8, max = 20, message = "새 비밀번호는 8자 이상, 20자 이하여야 합니다.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-        message = "비밀번호는 최소 8자 이상이어야 하며, 알파벳, 숫자, 특수문자를 포함해야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+        message = "비밀번호는 8자 이상 20자 이하여야 하며, 알파벳, 숫자, 특수문자를 포함해야 합니다.")
     private String password; // 평문 비밀번호
 
     @NotBlank(message = "닉네임은 공백일 수 없습니다.")
-    @Size(max = 8, message = "닉네임은 8자 이내여야 합니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임은 알파벳, 숫자, 한글만 포함할 수 있습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]{1,8}$",
+        message = "닉네임은 1자 이상 8자 이하여야 하며, 알파벳, 숫자, 한글만 포함할 수 있습니다.")
     private String nickname;
 
     @NotBlank(message = "이름은 공백일 수 없습니다.")
--- src/main/java/site/campingon/campingon/user/dto/UserUpdateRequestDto.java
+++ src/main/java/site/campingon/campingon/user/dto/UserUpdateRequestDto.java
@@ -2,7 +2,6 @@ package site.campingon.campingon.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,16 +10,14 @@ import lombok.Getter;
 public class UserUpdateRequestDto {
 
     @NotBlank(message = "닉네임은 공백일 수 없습니다.")
-    @Size(max = 8, message = "닉네임은 8자 이내여야 합니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임은 알파벳, 숫자, 한글만 포함할 수 있습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]{1,8}$", message = "닉네임은 알파벳, 숫자, 한글만 포함할 수 있습니다.")
     private String nickname;
 
     @NotBlank(message = "현재 비밀번호는 공백일 수 없습니다.")
     private String currentPassword; // 비밀번호 변경을 위해 기존 비밀번호 확인
 
 
-    @Size(min = 8, max = 20, message = "새 비밀번호는 8자 이상, 20자 이하여야 합니다.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-        message = "비밀번호는 최소 8자 이상이어야 하며, 알파벳, 숫자, 특수문자를 포함해야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+        message = "비밀번호는 8자 이상 20자 이하여야 하며, 알파벳, 숫자, 특수문자를 포함해야 합니다.")
     private String newPassword; // 새 비밀번호
 }
