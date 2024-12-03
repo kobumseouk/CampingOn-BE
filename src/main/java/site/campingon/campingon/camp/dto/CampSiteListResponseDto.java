4784e8a4b3bf8fae770845ece0edffd51d726fef
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-12-03 23:58:14 +0900

    refactor: #155 예약가능한 캠프사이트 조회 api 예약 -> 캠프도메인으로 이동
    - 예약조회용 메서드 가독성을 위해 서비스 분리
    - 예약조회에서 반환되는 캠프사이트반환객체 필드 일부수정

--- src/main/java/site/campingon/campingon/camp/dto/CampSiteListResponseDto.java
+++ src/main/java/site/campingon/campingon/camp/dto/CampSiteListResponseDto.java
@@ -16,12 +16,10 @@ public class CampSiteListResponseDto {
   private Integer maximumPeople;
   private Integer price;
   private Induty siteType;
-  private String indoor_facility;
+  private String indoorFacility;
 
-  @Builder.Default  // 기본값 자동 설정
-  private LocalTime checkInTime = LocalTime.of(15, 0);  // 15:00
+  private LocalTime checkInTime;
 
-  @Builder.Default  // 기본값 자동 설정
-  private LocalTime checkOutTime = LocalTime.of(11, 0);  // 11:00
+  private LocalTime checkOutTime;
 
 }
---
+++ src/main/java/site/campingon/campingon/camp/service/CampSiteReserveService.java
@@ -0,0 +1,43 @@
+package site.campingon.campingon.camp.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.campingon.campingon.camp.dto.CampSiteListResponseDto;
+import site.campingon.campingon.camp.entity.CampSite;
+import site.campingon.campingon.camp.mapper.CampSiteMapper;
+import site.campingon.campingon.camp.repository.CampSiteRepository;
+import site.campingon.campingon.common.exception.ErrorCode;
+import site.campingon.campingon.common.exception.GlobalException;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CampSiteReserveService {
+
+    private final CampSiteRepository campRepository;
+    private final CampSiteRepository campSiteRepository;
+    private final CampSiteMapper campSiteMapper;
+
+    // 캠핑장의 예약 가능한 SiteType 별 캠핑지 조회
+    @Transactional(readOnly = true)
+    public List<CampSiteListResponseDto> getAvailableCampSites(Long campId, LocalDate checkin, LocalDate checkout) {
+
+        campRepository.findById(campId)
+                .orElseThrow(() -> new GlobalException(ErrorCode.CAMP_NOT_FOUND_BY_ID));
+
+        List<CampSite> campSites = campSiteRepository.findAvailableCampSites(campId, checkin, checkout);
+
+        // 타입별 첫 번째 행 데이터만 가져오기
+        return campSites.stream()
+                .collect(Collectors.groupingBy(CampSite::getSiteType))
+                .values().stream()
+                .map(List::getFirst)
+                .map(campSiteMapper::toCampSiteListResponseDto)
+                .toList();
+    }
+}
