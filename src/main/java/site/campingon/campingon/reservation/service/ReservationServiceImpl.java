7e773c37c750a97ac62b1b81315a671336c3ef63
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-12-05 11:19:38 +0900

    fix: #202 예약가능한 캠핑지 조회 쿼리 오류 해결

--- src/main/java/site/campingon/campingon/reservation/service/ReservationServiceImpl.java
+++ src/main/java/site/campingon/campingon/reservation/service/ReservationServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import site.campingon.campingon.camp.dto.CampSiteResponseDto;
 import site.campingon.campingon.camp.entity.Camp;
 import site.campingon.campingon.camp.entity.CampSite;
 import site.campingon.campingon.camp.mapper.CampSiteMapper;
