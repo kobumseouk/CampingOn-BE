ed721af6dc8152b3d046a6d033bf1c2a8d21cadb
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-12-05 17:07:56 +0900

    Fix: #206 Entry point withelist 추가
    - api/users/me 빠져있던 부분 추가
    - 기존에 생성해서 넣던 Handler, EntryPoint Component 등록 후 Config에 추가
    
    Closes #206

--- src/main/java/site/campingon/campingon/common/jwt/CustomAccessDeniedHandler.java
+++ src/main/java/site/campingon/campingon/common/jwt/CustomAccessDeniedHandler.java
@@ -1,6 +1,7 @@
 package site.campingon.campingon.common.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -10,11 +11,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 import site.campingon.campingon.common.exception.ErrorCode;
 import site.campingon.campingon.common.exception.ErrorResponse;
 
 @Slf4j
 @RequiredArgsConstructor
+@Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     private final ObjectMapper objectMapper;
@@ -22,7 +25,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request,
         HttpServletResponse response,
-        AccessDeniedException accessDeniedException) throws IOException {
+        AccessDeniedException accessDeniedException) throws IOException, ServletException {
         log.error("No Authorities", accessDeniedException);
         log.error("Request Uri : {}", request.getRequestURI());
 
